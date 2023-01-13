c37b04d1353301f2c359446c48633cce6c53e046
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-01-13 03:05:45 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,12 @@
 import youtube_dl
 
 ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
+    'format': 'bestaudio/mp3',
     'outtmpl':'%(title)s.%(ext)s',
-    'quiet': True
 }
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=XXXXXXXXXXX'])
+try:
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['https://www.youtube.com/watch?v=0KaWvzuYFB8'])
+except Exception as e:
+    print(f'An error occured: {e}')
